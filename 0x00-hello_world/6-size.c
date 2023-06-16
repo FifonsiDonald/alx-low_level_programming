001dac258d63b93af018c2f9dbef38cc35813280
FifonsiDonald <dfifonsi@st.ug.edu.gh>
2023-06-15 19:16:03 -0700

    size of function

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point for my program
+ *
+ * my program prints out the size of various computer types it is run on
+ * Return: Always return 0 (success)
+ */
+int main(void)
+{
+printf("Size of a char: %zu byte(s)\n", sizeof(char));
+printf("Size of an int: %zu byte(s)\n", sizeof(int));
+printf("Size of a float: %zu byte(s)\n", sizeof(float));
+printf("Size of a double: %zu byte(s)\n", sizeof(double));
+printf("Size of a long: %zu byte(s)\n", sizeof(long));
+printf("Size of a long long: %zu byte(s)\n", sizeof(long long));
+
+return (0);
+}
