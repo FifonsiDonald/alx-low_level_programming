5361371c26692853accba49f06db875216267687
FifonsiDonald <dfifonsi@st.ug.edu.gh>
2023-06-15 20:00:38 -0700

    dora

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point for the program
+ *
+ * Description: This function serves as the entry point for program execution.
+ *              It is responsible for [briefly describe the main functionality
+ *              of your program].
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+write(2,
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"'59);
+return(1);
+}
