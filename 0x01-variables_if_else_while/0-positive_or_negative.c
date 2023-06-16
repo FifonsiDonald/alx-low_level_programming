5e45a07e98c2e823fcb6d943fa8b630b5312d33f
FifonsiDonald <dfifonsi@st.ug.edu.gh>
2023-06-16 15:47:41 -0700

    if statement used to check if number is positive, negative or zero

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point of the program.
+ *
+ * Return: 0 on successful execution.
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else
+	{
+		printf("%d is 0\n", n);
+	}
+
+	return (0);
+}
+
