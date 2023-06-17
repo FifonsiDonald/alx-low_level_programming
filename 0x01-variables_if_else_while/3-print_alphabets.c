002158937adca9afc731da9c3ff3493734c35420
FifonsiDonald <dfifonsi@st.ug.edu.gh>
2023-06-16 17:26:09 -0700

    alphabets upper and lower case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point of the program.
+ *
+ * Return: 0 on successful execution.
+ */
+int main(void)
+{
+        char letter;
+
+        for (letter = 'a'; letter <= 'z'; letter++)
+        {
+                putchar(letter);
+        }
+	for (letter = 'A'; letter <= 'Z'; letter++)
+        {
+                putchar(letter);
+        }
+	putchar('\n');
+        return (0);
+}
