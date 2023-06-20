9d4e2ffd03e60a1ccd79169f70f2788091c68f5c
FifonsiDonald <dfifonsi@st.ug.edu.gh>
2023-06-20 03:16:49 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point of the program.
+ *
+ * Return: 0 on successful execution.
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+/**
+ * print_alphabet - prints alphabet
+ */
+void print_alphabet(void)
+{
+	char a
+	for (a = 'a'; a <= 'z'; ++a)
+	{
+		putchar(a);
+	}
+	putchar(\n)
+}
