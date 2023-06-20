540bf343587783eb7070205494540119f15eb1db
FifonsiDonald <dfifonsi@st.ug.edu.gh>
2023-06-20 15:43:53 -0700

    24 heures chrono

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *  jack_bauer - prints all time in a day
+ */
+void jack_bauer(void)
+{
+	int i, j;
+
+	i = 0;
+
+	while (i < 24)
+	{
+		j = 0;
+		while (j < 60)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((i / 10) + '0');
+			_putchar('\n');
+			j++;
+		}
+		i++;
+	}
+}
