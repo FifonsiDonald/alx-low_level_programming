39ec108154c937692ea302bf88891b039d810347
FifonsiDonald <dfifonsi@st.ug.edu.gh>
2023-06-20 14:22:15 -0700

    absolute value

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * print_putchar - prints putchar
- *
- *
+ * print_sign - prints sign of an int
+ *@n: the nnumber's sign we determine
+ *Return: 0 for 0 ,-1 for negatives and 1 for positives
  */
 int print_sign(int n)
 {
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *_abs - prints basolute value
+ *Return: 0 if ran
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
