4d7714f84e5a40ec276c0286213910f1c93845d5
FifonsiDonald <dfifonsi@st.ug.edu.gh>
2023-06-20 02:43:15 -0700

    fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <unistd.h>
+#include "main.h"
 
 /* betty style doc for function main goes there */
 /**
@@ -10,7 +10,11 @@
  */
 int main(void)
 {
-	printf("_putchar");
+	print_putchar();
 	return (0);
+}
+void print_putchar()
+{
+	printf("_putchar\n");
 }
 
