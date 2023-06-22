8dd953efbb959df030c597fa517544e7b58db303
FifonsiDonald <dfifonsi@st.ug.edu.gh>
2023-06-22 06:20:18 -0700

    print numbers 1-14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 1-14 x10
+ */
+void more_numbers(void)
+{
+	int num2;
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		for (num2 = 0; num2 < 15; num2++)
+		{
+			(num2 > 9) ? _putchar((num2 % 10) + '0') : 0;
+			_putchar((num2 / 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
