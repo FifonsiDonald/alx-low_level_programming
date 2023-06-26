f4c50381c3b07f381ffab7c0621b88840248c91d
FifonsiDonald <dfifonsi@st.ug.edu.gh>
2023-06-26 07:57:35 -0700

    swap function

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *swap_int - swaps values of a and b
+ *@a: memory location
+ *@b: memory location
+ *Return: void
+ *
+ */
+void swap_int(int *a, int *b);
+{
+	int newa = *b;
+	int newb = *a;
+
+	*b = newb;
+	*a = newa;
+}
