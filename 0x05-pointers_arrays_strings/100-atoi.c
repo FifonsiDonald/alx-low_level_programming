73cba2ff70be22379a2339d6d5e968f9360bad27
FifonsiDonald <dfifonsi@st.ug.edu.gh>
2023-06-26 19:34:34 -0700

    more fixes

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,36 +8,34 @@
 int _atoi(char *s)
 {
 	int sign = 1;
-    int result = 0;
-    int digit;
-    int INT_MAX = 2147483647;
-    int INT_MIN = -2147483648;
+	int result = 0;
+	int digit;
 
-    while (*s == ' ')
-        s++;
+	while (*s == ' ')
+		s++;
 
-    if (*s == '-' || *s == '+')
-    {
-        if (*s == '-')
-            sign = -1;
-        s++;
-    }
+	if (*s == '-' || *s == '+')
+	{
+		if (*s == '-')
+			sign = -1;
+		s++;
+	}
 
-    while (*s >= '0' && *s <= '9')
-    {
-        digit = *s - '0';
+	while (*s >= '0' && *s <= '9')
+	{
+		digit = *s - '0';
 
-        if (result > INT_MAX / 10 || (result == INT_MAX / 10 && digit > INT_MAX % 10))
-        {
-            if (sign == 1)
-                return INT_MAX;
-            else
-                return INT_MIN;
-        }
+		if (result > INT_MAX / 10 || (result == INT_MAX / 10 && digit > INT_MAX % 10))
+		{
+			if (sign == 1)
+				return INT_MAX;
+			else
+				return INT_MIN;
+		}
 
-        result = result * 10 + digit;
-        s++;
-    }
+		result = result * 10 + digit;
+		s++;
+	}
 
-    return (0);
+	return (0);
 }
