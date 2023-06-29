aed2dc18afc5f66789f0b01d4e47d5c7f8ec0eb0
FifonsiDonald <dfifonsi@st.ug.edu.gh>
2023-06-29 14:04:46 -0700

    fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,19 +8,22 @@
 
 char *_strcat(char *dest, char *src)
 {
-	char *dest_ptr = dest;
+	int i, j;
 
-	while (*dest_ptr != "\0")
+	i = 0;
+	while (dest[i] != "\0")
 	{
-		dest_ptr++;
+		i++;
 	}
+	j = 0;
 
-	while (*src != "\0")
+	while (src[j] != "\0")
 	{
-		*dest_ptr = *src;
-		dest_ptr++;
-		src++;
+		dest[i] = src[j];
+		j++;
+		i++;
 	}
-	*dest_ptr = "\0";
+	i++
+	dest[i] = "\0";
 	return (dest);
 }
