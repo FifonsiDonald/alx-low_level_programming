837393e22576e5d619b485839ecd3b9e38461c78
FifonsiDonald <dfifonsi@st.ug.edu.gh>
2023-07-03 16:03:06 -0700

    memory copy

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,15 +1,18 @@
 #include "main.h"
 /**
- *_memset - copies bytes
+ *_memset - fills memory with a constant byte 
+ *@n: integer
  *@s: location
- *@b: location for string two
+ *@b: other location
  *Return: *s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
+	unsigned int i;
+
 	for (i = 0; i < n; i++)
 	{
-		s[i] = b[i];
+		s[i] = b;
 	}
 	return (s);
 }
---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_memset - copies bytes
+ *@n: integer
+ *@s: location
+ *@b: location for string two
+ *Return: *s
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+        unsigned int i;
+
+        for (i = 0; i < n; i++)
+        {
+                dest[i] = src[i];
+        }
+        return (dest);
+}
