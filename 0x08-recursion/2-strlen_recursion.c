03a5f5bd7435a1ba91d2c11ef351bf5239c60052
FifonsiDonald <dfifonsi@st.ug.edu.gh>
2023-07-05 13:29:24 -0700

    first commit

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_puts_recursion - uses recursion
+ *
+ * @s: string to recurse through
+ */
+void _puts_recursion(char *s)
+{
+	if(*s)
+	{
+		_puts_recursion(s++);
+		_putchar(*s)
+	}
+}
