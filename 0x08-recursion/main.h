3d25c52e7531c6b3513d8f20521932368054d78a
FifonsiDonald <dfifonsi@st.ug.edu.gh>
2023-07-05 18:11:58 -0700

    fix

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,8 +11,8 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
-int is_prime(int n, int start);
 int check_palindrome(char *s, int len, int index);
 int real_sqrt(int n, int i);
+int is_prime(int n, int start);
 
 #endif /* MAIN_H */
