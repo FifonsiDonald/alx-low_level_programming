0f7f0b444f229d8a48df51e2486d992ae94a22c1
FifonsiDonald <dfifonsi@st.ug.edu.gh>
2023-07-08 18:04:54 -0700

    making change

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints name
+ * @argc: number of command lne arguments
+ * @argv: array of cmd line argumets.
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints arguments received
+ * @argv: array that contains the program command line arguments.
+ * @argc:number of command line arguments.
+ * Return: 0 after success.
+ */
+int main(int argc, char *argv[])
+{
+	for (int i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
