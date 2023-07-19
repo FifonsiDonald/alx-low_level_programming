c35df67389d7f49d9bb584115abacdc2bbbc8f97
FifonsiDonald <dfifonsi@st.ug.edu.gh>
2023-07-18 18:50:09 -0700

    new approach

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,42 +1,20 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <string.h>
-
+#include <stdio.h>
 /**
- * new_dog - Create a new dog.
- * @name: Name of the dog.
- * @age: Age of the dog.
- * @owner: Owner of the dog.
- * Return: Pointer to the new dog (dog_t), or NULL if function fails.
+ * new_dog - creates dog object
+ * @name: dog name
+ * @age: age of dog
+ * @owner: owner of dog
+ * Return: dog object, NULL if fails
  **/
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog = (dog_t *)malloc(sizeof(dog_t));
 
-	if (name == NULL || age < 0 || owner == NULL)
+
+	if (hobbes == NULL)
 		return (NULL);
 
-	if (new_dog == NULL)
-	{
-		return (NULL);
-	}
 
-	new_dog->name = strdup(name);
-	if (new_dog->name == NULL)
-	{
-		free(new_dog);
-		return (NULL);
-	}
-	new_dog->owner = strdup(owner);
-	if (new_dog->owner == NULL)
-	{
-		free(new_dog->name);
-		free(new_dog);
-		return (NULL);
-	}
-
-	new_dog->age = age;
-
-	return (new_dog);
+	return (hobbes);
 }
-
