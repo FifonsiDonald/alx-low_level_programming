a6e072a29447ca055c1c9389cc11c7d3d9da15ea
FifonsiDonald <dfifonsi@st.ug.edu.gh>
2023-07-18 17:15:23 -0700

    fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,5 +20,12 @@ struct dog
 /* Define a new type dog_t as a new name for struct dog */
 typedef struct dog dog_t;
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+int _putchar(char c);
+
 #endif /* DOG_H */
 
