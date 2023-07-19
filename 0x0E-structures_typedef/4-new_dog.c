2f76e5b79753eae7157fc0593fe0607db1597dbb
FifonsiDonald <dfifonsi@st.ug.edu.gh>
2023-07-18 18:26:04 -0700

    warning fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,9 +11,10 @@
  **/
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	dog_t *new_dog = (dog_t *)malloc(sizeof(dog_t));
+
 	if (name == NULL || owner == NULL)
 		return (NULL);
-	dog_t *new_dog = (dog_t *)malloc(sizeof(dog_t));
 
 	if (new_dog == NULL)
 	{
