f12e43b9acf63f7f9e2d4fd4219c3246dcd9dc90
FifonsiDonald <dfifonsi@st.ug.edu.gh>
2023-07-18 18:09:04 -0700

    some more changes

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,37 +1,48 @@
 #include "dog.h"
 #include <stdlib.h>
 #include <string.h>
+
 /**
- * new_dog - new dog created
- * @name: name
- * @age: age
- * @owner: pointer to owner
+ * new_dog - Create a new dog.
+ * @name: Name of the dog.
+ * @age: Age of the dog.
+ * @owner: Owner of the dog.
  * Return: Pointer to the new dog (dog_t), or NULL if function fails.
  **/
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	if (name == NULL || owner == NULL)
-		return (NULL);
+    if (name == NULL || owner == NULL)
+        return (NULL);
 
-	dog_t *new_dog = (dog_t *)malloc(sizeof(dog_t));
+    // Declare the pointer first, then allocate memory
+    dog_t *new_dog = (dog_t *)malloc(sizeof(dog_t));
+    if (new_dog == NULL)
+    {
+        // Handle memory allocation failure
+        return NULL;
+    }
 
-	if (new_dog == NULL)
-	{
-		return (NULL);
-	}
-	new_dog->owner = strdup(name);
-	if (new_dog->name == NULL)
-	{
-		free(new_dog);
-		return (NULL);
-	}
-	new_dog->owner = strdup(owner);
-	if (new_dog->owner == NULL)
-	{
-		free(new_dog->name);
-		free(new_dog);
-		return (NULL);
-	}
-	new_dog->age = age;
-	return (new_dog);
+    // Copy the name
+    new_dog->name = strdup(name);
+    if (new_dog->name == NULL)
+    {
+        // Handle memory allocation failure
+        free(new_dog);
+        return NULL;
+    }
+
+    // Copy the owner
+    new_dog->owner = strdup(owner);
+    if (new_dog->owner == NULL)
+    {
+        // Handle memory allocation failure
+        free(new_dog->name);
+        free(new_dog);
+        return NULL;
+    }
+
+    new_dog->age = age;
+
+    return new_dog;
 }
+
