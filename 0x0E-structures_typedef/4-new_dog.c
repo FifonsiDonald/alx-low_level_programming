86417e965e238afa6311e2c0f18e62bea3a6a20e
FifonsiDonald <dfifonsi@st.ug.edu.gh>
2023-07-18 18:56:53 -0700

    another approach

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,7 @@
 #include "dog.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 /**
  * new_dog - creates dog object
  * @name: dog name
@@ -10,11 +11,16 @@
  **/
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	dog_t *d = malloc(sizeof(dog_t));
 
-
-	if (hobbes == NULL)
+	if (!d)
 		return (NULL);
-
-
-	return (hobbes);
+	d->name = _strdup(name);
+	if (name && !d->name)
+		return (free(d), NULL);
+	d->owner = _strdup(owner);
+	if (owner && !d->owner)
+		return (free(d->name), free(d), NULL);
+	d->age = age;
+	return (d);
 }
