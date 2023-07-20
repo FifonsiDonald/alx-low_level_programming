03bb5b3748fda82a641e95c5fcdc98e6d70ed445
FifonsiDonald <dfifonsi@st.ug.edu.gh>
2023-07-19 17:17:50 -0700

    first commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_name - prints name 
+ * @name - name to print
+ * @f - function that prints
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name || !f)
+		return;
+
+	f(name);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - adds 
+ * @a: int to add
+ * @b: int to add to
+ * Return: sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - substracts 
+ * @a: int to substract from
+ * @b: int to substract
+ * Return: subtraction of a and b
+ */ 
+int op_sub(int a, int b)
+{       
+	return (a - b);
+}
+/**
+ * op_mul - multiplies
+ * @a: int to multiply
+ * @b: int to multiply
+ * Return: multiplication of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divides
+ * @a: int to divide by
+ * @b: int to divide
+ * Return: division of a and b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - mods
+ * a: int to mod by
+ * @b: int to mod
+ * Return: mod of a and b
+ */
+int op_mod(int a, int b)
+{
+        return (a % b);
+}
