c9b6197e5c48de4fa96dec87218155b758537119
FifonsiDonald <dfifonsi@st.ug.edu.gh>
2023-07-19 18:02:00 -0700

    more changes

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,10 +9,12 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	if (array == NULL || action == NULL)
+	unsigned int i;
+
+	if (!array || !action)
 		return;
 
-	for (size_t i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
 	}
