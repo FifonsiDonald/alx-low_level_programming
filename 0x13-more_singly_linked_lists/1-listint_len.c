c72db219f7b866a6cda70b128081dc64b949ce99
FifonsiDonald <dfifonsi@st.ug.edu.gh>
2023-07-31 16:51:10 -0700

    number of nodes node_n

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * listint_len - prints the number of elements in listint_t list
+ * @h: constant pointer type list_t
+ *
+ * Return: the numbers of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t node_n;
+	
+	node_n = 0;
+	while (h != NULL)
+	{
+		node_n++;
+		h = h->next;
+	}
+	return (node_n);
+}
