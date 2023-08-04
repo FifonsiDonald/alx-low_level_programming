6574fcd10d8239a00ca8bd4f448a1b5a24afe963
FifonsiDonald <dfifonsi@st.ug.edu.gh>
2023-08-03 19:07:53 -0700

    commit

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_binary - prints the binary representation of a number.
+ * @n: integer n.
+ * Result: void.
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+	{
+		n >>= 1;
+		print_binary(n);
+	}
+	else
+	{
+		_putchar((n & 1) + '0');
+	}
+}
---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * set_bit - sets the value of a bit to 1 at a given index.
+ * @index: the index, starting from 0 of the bit you want to set
+ * @n: number.
+ * Return: 1 if it worked, or -1 if an error occurred.
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int bit;
+
+	if (index > 63)
+		return (-1);
+
+	bit = 1 << index;
+	*n = (*n | bit);
+
+	return (1);
+
+}
