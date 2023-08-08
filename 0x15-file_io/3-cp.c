ecc0ca287789edff3de36bf25f8dbb3045f350a3
FifonsiDonald <dfifonsi@st.ug.edu.gh>
2023-08-07 19:46:18 -0700

    betty fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,14 +18,12 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	source_fd = open(*(argv + 1), O_RDONLY);
 	if (source_fd == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", *(argv + 1));
 		exit(98);
 	}
-
 	dest_fd = open(*(argv + 2), O_TRUNC | O_CREAT | O_WRONLY, 0664);
 	while ((bytes_read = read(source_fd, buffer, 1024)) > 0)
 	{
@@ -35,20 +33,16 @@ int main(int argc, char *argv[])
 			exit(99);
 		}
 	}
-
 	if (bytes_read < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", *(argv + 1));
 		exit(98);
 	}
-
 	close_source = close(source_fd);
 	if (close_source < 0)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", source_fd), exit(100);
-
 	close_dest = close(dest_fd);
 	if (close_dest < 0)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", dest_fd), exit(100);
-
 	return (0);
 }
